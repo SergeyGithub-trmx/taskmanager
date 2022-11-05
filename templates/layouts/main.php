eb4d9a00273d045af9d8e22270cf2470d132c1e7
AUGMENTARO <109080715+AUGMENTARO@users.noreply.github.com>
2022-11-06 00:44:00 +1000

    Add files via upload

---
+++ templates/layouts/main.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+    <meta charset="UTF-8">
+    <title><?= $title ?></title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+    <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css'>
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans'>
+    <link rel="stylesheet" href="<?= $css_href ?>">
+</head>
+<body>
+    <main>
+        <?= $page_content ?>
+    </main>
+</body>
+</html>
